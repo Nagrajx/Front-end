c47091dca79b3ecb4193f78ed36968cfe1e2279b
Nagrajx <satishnagraj54@gmail.com>
2026-07-14 23:42:29 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,90 @@
 import { Routes, Route } from "react-router-dom";
 
 import Register from "./pages/Register";
-import Login from "./pages/login"
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+
+// Customer Pages
 import CustomerList from "./pages/CustomerList";
 import AddCustomer from "./pages/AddCustomer";
 import EditCustomer from "./pages/EditCustomer";
+
+// Employee Pages
 import AddEmployee from "./pages/AddEmployee";
 import EmployeeList from "./pages/EmployeeList";
 import EditEmployee from "./pages/EditEmployee";
 
+
 function App() {
   return (
     <Routes>
-      {/* Default Route */}
-  
-      <Route path="/home" element={<Register />} > </Route>
-      
-      {/* Register */}
-      <Route path="/register" element={<Register />} />
 
-      Login
-      <Route path="/login" element={<Login />} />
+      {/* Default */}
+      <Route 
+        path="/" 
+        element={<Register />} 
+      />
 
-      <Route path="/dashboard" element={<Dashboard />} />
 
-      <Route path="/customers" element={<CustomerList />} />
-      <Route path="/customer/add" element={<AddCustomer />} />
-      <Route path="/customer/edit/:id" element={<EditCustomer />} />
+      {/* Auth Routes */}
+      <Route
+        path="/register"
+        element={<Register />}
+      />
 
       <Route
-          path="/employee/add"
-          element={<AddEmployee />}
-        />
+        path="/login"
+        element={<Login />}
+      />
 
-        <Route path="/employees" element={<EmployeeList />} />
 
-          <Route
-          path="/employee/edit/:id"
-          element={<EditEmployee />}
-        />
+      {/* Dashboard */}
+      <Route
+        path="/dashboard"
+        element={<Dashboard />}
+      />
+
+
+      {/* Customer Routes */}
+      <Route
+        path="/customers"
+        element={<CustomerList />}
+      />
+
+      <Route
+        path="/customer/add"
+        element={<AddCustomer />}
+      />
+
+      <Route
+        path="/customer/edit/:id"
+        element={<EditCustomer />}
+      />
+
+
+      {/* Employee Routes */}
+      <Route
+        path="/employees"
+        element={<EmployeeList />}
+      />
+
+      <Route
+        path="/employee/add"
+        element={<AddEmployee />}
+      />
+
+      <Route
+        path="/employee/edit/:id"
+        element={<EditEmployee />}
+      />
+
+
+      {/* 404 */}
+      <Route
+        path="*"
+        element={<Register />}
+      />
+
     </Routes>
   );
 }
