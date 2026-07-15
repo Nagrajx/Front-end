b3bd31b9853c512d4759ebc45b7c1e6c48c41d74
Nagrajx <satishnagraj54@gmail.com>
2026-07-15 11:27:29 +0530

    fix route

--- src/services/customerApi.js
+++ src/services/customerApi.js
@@ -1,10 +1,8 @@
 import axios from "axios";
-import employee from "./employeeApi";
-
 
 const customer = axios.create({
-    baseURL: import.meta.env.VITE_API_URL/customer,
-    withCredentials: true
+  baseURL: `${import.meta.env.VITE_API_URL}/customer`,
+  withCredentials: true,
 });
 
 export default customer;
