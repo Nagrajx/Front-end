a0c1a2fd591c94abebbda83f8911a29af99da1a1
Nagrajx <satishnagraj54@gmail.com>
2026-07-15 15:24:45 +0530

    fix route

--- src/services/customerApi.js
+++ src/services/customerApi.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+
+// change on route
 const customer = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/customer`,
   withCredentials: true,
--- src/services/employeeApi.js
+++ src/services/employeeApi.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// also change on route
 const employee = axios.create({
   baseURL: import.meta.env.VITE_API_URL + "/employee",
   withCredentials: true,
