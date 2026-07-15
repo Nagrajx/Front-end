e06b425c1e587713211b4c4a72e1b7e316c2cbaa
Nagrajx <satishnagraj54@gmail.com>
2026-07-15 10:10:49 +0530

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-import axios from "axios";
+import axios from "axios"
+require("dotenv").config()
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
--- src/services/employeeApi.js
+++ src/services/employeeApi.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+require("dotenv").config();
 
 const employee = axios.create({
     baseURL: `${import.meta.env.VITE_API_URL}/employee`,
